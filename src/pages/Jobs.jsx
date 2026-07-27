cb681d75f18d35ed55e84ea19c18408fd19980d5
royalaryan93 <2k22cscys2211662@gmail.com>
2026-07-27 19:54:31 +0530

    Update Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -19,7 +19,7 @@ localStorage.getItem("token")
 );
 async function DeleteJob(id) {
     try {
-        await axios.delete(`https://job-portal-backend-2q57.onrender.com/${id}`,
+        await axios.delete(`https://job-portal-backend-2q57.onrender.com/jobs/${id}`,
         
         {
              headers: {
@@ -40,7 +40,7 @@ async function DeleteJob(id) {
 
             const response = await axios.get(
 
-                "https://job-portal-backend-2q57.onrender.com",
+                "https://job-portal-backend-2q57.onrender.com/jobs",
                  {
                 headers: {
 
